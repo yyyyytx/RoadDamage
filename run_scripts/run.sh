73050a75e38a83e5f2ae5aa20a5509bd59a2f568
ytx <40585021+yyyyytx@users.noreply.github.com>
2023-03-15 10:32:15 +0800

    Delete run_scripts directory

--- run_scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CONFIG=/home/liu/ytx/road_damage/configs/ACTL_final.py
-WORK_DIR=/home/liu/ytx/road_damage/outputs
-
-
-
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=9500 \
- tools/train.py ${CONFIG} --seed 0 --work-dir=${WORK_DIR} --launcher pytorch
